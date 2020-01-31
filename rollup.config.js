b2d91d847ccfaf4090d548a5a14563267fe9d7c4
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2020-01-31 14:13:52 +0100

    Partner pages

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 const {Logger, LogLevel} = require('plop-logger');
-const {colorEmojiConfig} = require('plop-logger/lib/extra/colorEmojiConfig');
+const {colorEmojiConfig} = require('plop-logger');
 
 Logger.config = colorEmojiConfig;
 const logger = Logger.getLogger('icons');
